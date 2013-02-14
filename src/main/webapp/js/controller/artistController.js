f9d0c79999d367617503ade4657f5e5708fb2a6c
Charlie Midtlyng <Charlie.Midtlyng@BEKK.no>
2013-02-14 15:34:41 +0100

    Solution task 3

--- src/main/webapp/js/controller/artistController.js
+++ src/main/webapp/js/controller/artistController.js
@@ -1,15 +1,35 @@
 define(['jquery', 'underscore', 'when'], function($, _, when) {
 
     var controller = function() {
+            function logerror(oops) {
+                console.log(oops);
+            }
 
             function addImage(imageUrl) {
                 return '<img src=' + imageUrl + ' />';
             }
 
+            function fetchSimilarArtist(artist) {
+                return $.ajax({
+                    url: '/api/similar-' + artist.mbid + '.json',
+                    dataType: 'json'
+                });
+            }
+
+            function renderSimilarArtists(allSimilarArtists) {
+                _.each(allSimilarArtists, function(similarArtists) {
+                    var mbid = similarArtists.artistMbid;
+                    _.each(similarArtists.similar, function(artist) {
+                        $('<div>Navn: ' + artist.name + ':' + artist.match + '</div>').appendTo('li#' + mbid);
+                    });
+                });
+            }
+
             function renderTopArtists(artists) {
                 _.each(artists, function(artist) {
-                    $('<li><span>Navn: ' + artist.name + '</span>' + addImage(artist.image) + '</li>').appendTo('ul');
+                    $('<li id="' + artist.mbid + '"><span>Navn: ' + artist.name + '</span>' + addImage(artist.image) + '</li>').appendTo('ul');
                 });
+                return artists;
             }
 
             var fetchTopArtists = function() {
@@ -21,14 +41,23 @@ define(['jquery', 'underscore', 'when'], function($, _, when) {
 
             var renderUl = function() {
                     var deferred = when.defer();
+
                     function resolve() {
                         deferred.resolve($('<ul></ul>').appendTo(controller.elem));
                     }
                     setTimeout(function() {
                         resolve();
-                    }, 3000);
+                    }, 1000);
                     return deferred.promise;
                 };
+
+            var loadAllSimilarArtists = function(artists) {
+                var similarArtistDeferred = [];
+                        _.each(artists, function(artist) {
+                            similarArtistDeferred.push(fetchSimilarArtist(artist));
+                        });
+                return when.all(similarArtistDeferred);
+            };
 
             // Public functions
             return {
@@ -38,9 +67,10 @@ define(['jquery', 'underscore', 'when'], function($, _, when) {
                 },
                 render: function() {
                     $(controller.elem).empty();
-                    when(renderUl())
-                    .then(fetchTopArtists)
-                    .then(renderTopArtists);
+                    when(renderUl()).then(fetchTopArtists).then(renderTopArtists).then(function(artists) {
+                        loadAllSimilarArtists(artists).then(renderSimilarArtists);
+                        
+                    });
                 }
             };
         };
