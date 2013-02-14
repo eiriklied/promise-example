4027863c0f83e2897e5f90c7bce6bb2e7bad8c60
Charlie Midtlyng <Charlie.Midtlyng@BEKK.no>
2013-02-14 12:46:28 +0100

    Task1 finished

--- src/main/webapp/js/controller/artistController.js
+++ src/main/webapp/js/controller/artistController.js
@@ -1,56 +1,59 @@
 define(['jquery', 'underscore', 'when'], function($, _, when) {
 
-	var controller = function() {
+    var controller = function() {
 
-			function addImage(imageUrl) {
-				return '<img src=' + imageUrl + ' />';
-			}
+            function addImage(imageUrl) {
+                return '<img src=' + imageUrl + ' />';
+            }
 
-			function renderTopArtists(artist) {
-				$('<li><span>Navn: ' + artist.name + '</span>' + addImage(artist.image) + '</li>').appendTo('ul');
-			}
+            function renderTopArtists(artist) {
+                $('<li><span>Navn: ' + artist.name + '</span>' + addImage(artist.image) + '</li>').appendTo('ul');
+            }
 
-			var renderUl = function() {
-					setTimeout(function() {
-						$('<ul></ul>').appendTo(controller.elem);
-					}, 3000);
-				};
-			
-			/** Hack below here! **/
-			var fetchTopArtists = function() {
-					$.ajax({
-						url: '/api/topartists.json',
-						dataType: 'json',
-						success: function(artists) {
-							_.each(artists, function(artist) {
-								renderTopArtists(artist);
-							});
-						},
-						error: function(dog) {
-							$('<li>Ooops!</li>').appendTo('ul');
-						}
+            var fetchTopArtists = function() {
+                    $.ajax({
+                        url: '/api/topartists.json',
+                        dataType: 'json',
+                        success: function(artists) {
+                            _.each(artists, function(artist) {
+                                renderTopArtists(artist);
+                            });
+                        },
+                        error: function(dog) {
+                            $('<li>Ooops!</li>').appendTo('ul');
+                        }
 
-					});
-				};
+                    });
+                };
 
-			// Public functions
-			return {
-				init: function(elem) {
-					controller.elem = $(elem);
-					return this;
-				},
-				render: function() {
-					$(controller.elem).empty();
-					renderUl();
-					fetchTopArtists();
-				}
-			};
-		};
+            /** Hack below here! **/
+            var renderUl = function() {
+                    function appendUl() {
+                        $('<ul></ul>').appendTo(controller.elem);
+                    }
+                    setTimeout(function() {
+                        appendUl();
+                    }, 3000);
+                };
+
+            // Public functions
+            return {
+                init: function(elem) {
+                    controller.elem = $(elem);
+                    return this;
+                },
+                render: function() {
+                    $(controller.elem).empty();
+                    renderUl();
+                    fetchTopArtists();
+                }
+            };
+        };
 
 
-	// init function
-	return function(elem, options) {
-		return Object.create(controller()).init(elem, options);
-	};
+    // init function
+    return function(elem, options) {
+        return Object.create(controller()).init(elem, options);
+    };
 
 });
